adfdd549eb9f37c3ea411a8f5ec7bc6300d00cc5
Rohan <rohan26ir@gmail.com>
2025-03-08 23:34:52 +0600

    update

--- src/app/components/Shared/Navbar.jsx
+++ src/app/components/Shared/Navbar.jsx
@@ -7,12 +7,14 @@ const Navbar = () => {
       <div className='w-11/12 mx-auto'>
 
       <div className='flex justify-between items-center'>
-        <div className='flex items-center gap-1'>
+        {/* first sction */}
+        <div className='flex items-center gap-1 cursor-pointer'>
           <div className='text-red-600'><SiMatternet /></div>
           <h1 className='text-xl'>Shadow<span className='font-bold'>Net</span></h1>
         </div>
+        {/* middle section */}
         <div className='hidden md:block'>
-          <ul className='flex gap-5 text-gray-600'>
+          <ul className='flex gap-5 text-gray-600 cursor-pointer'>
             <li>Home</li>
             <li>About</li>
             <li>Features</li>
@@ -20,13 +22,13 @@ const Navbar = () => {
             <li>Download</li>
           </ul>
         </div>
-        {/* 3rd section */}
+        {/* end section */}
         <div className='flex gap-2 items-center'>
           <div className='hidden md:block'>
-            <button className='px-3 py-1 rounded-3xl font-bold'>Sign In</button>
+            <button className='px-3 py-1 rounded-3xl font-bold cursor-pointer'>Sign In</button>
           </div>
           <div>
-            <button className='border-2 border-red-600 text-red-600 font-bold px-3 py-1 rounded-3xl'>Sign Up</button>
+            <button className='border-2 border-red-600 text-red-600 font-bold px-3 py-1 rounded-3xl cursor-pointer'>Sign Up</button>
           </div>
         </div>
       </div>
